33850524e4467099966541f124e42004d1f147c5
Kophi4065 <alartnation@gmail.com>
2025-12-12 04:39:21 -0500

    Delete commands/antidelete.js

--- commands/antidelete.js
+++
@@ -1,8 +0,0 @@
-export default {
-  name: "antidelete",
-  desc: "anti-delete handler placeholder",
-  exec: async ({ sock, m, from, store }) => {
-    // Global anti-delete is handled in index.js messages.update
-    await sock.sendMessage(from, { text: "Anti-delete is active." });
-  }
-};
