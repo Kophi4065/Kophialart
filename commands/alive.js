5487b0f712d58f941a17c4ffdff31ce2f7350716
Kophi4065 <alartnation@gmail.com>
2025-12-12 04:50:08 -0500

    Delete commands/alive.js

--- commands/alive.js
+++
@@ -1,8 +0,0 @@
-export default {
-  name: "alive",
-  desc: "Check bot is alive",
-  exec: async ({ sock, m, from, config }) => {
-    const txt = `🔥 ${config.botName} is online!\nOwner: ${config.ownerName} (${config.ownerNumber})`;
-    await sock.sendMessage(from, { text: txt });
-  }
-};
